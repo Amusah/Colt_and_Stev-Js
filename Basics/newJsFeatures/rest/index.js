2fe0571c83d7d0ceb14f328221454f7df759d8dc
Amusah <henryamusah@gmail.com>
2023-03-22 15:56:55 +0000

    The rest operator

--- Basics/newJsFeatures/rest/index.js
+++ Basics/newJsFeatures/rest/index.js
@@ -26,3 +26,42 @@ arguments has initially been passed into function
 
 
                 /*********REST PARAMETERS*********/
+/*
+rest... operator, similar to spread but rest
+collects individual elements/arguments into a
+single array
+
+unlike the arguments keyword, rest... collects
+the remaining arguments leaving out the initially
+passed arguments and work with it
+*/
+
+let arr = (...nums) => {
+    console.log(nums);
+    console.log(nums.length);
+
+    // let facto = 1;
+    // for(let i of nums){
+    //     facto *= i;
+    // }
+    //return facto;
+    return nums.reduce((result, value) => result * value);
+}
+console.log(arr(1,2,3,4,5));
+
+
+function fullName(first, last, ...titles){
+    console.log('firstName: ', first);
+    console.log('last: ', last);
+    console.log('titles: ',titles);
+
+    return titles.every(title => title[title.length - 1] === '.');
+}
+
+console.log(fullName('Henry', 'Amusah', 'Mr.', 'Prof.', 'Eng.'));
+
+
+const multiply = (...nums) => (
+    nums.reduce((total, curVal) => total * curVal)
+);
+console.log(multiply(1,2,3,4,5));
