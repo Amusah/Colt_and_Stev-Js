523e30465288794fdc6c5f6df2536778af536bb0
Amusah <henryamusah@gmail.com>
2023-03-22 13:03:01 +0000

    Spread operator

--- Basics/newJsFeatures/spread/index.js
+++ Basics/newJsFeatures/spread/index.js
@@ -12,3 +12,40 @@ function printItems(a, b, c, d){
 }
 //printItems(nums); // a [5 , 4, 7, 8, 9, 2]; a is seen as a single argument
 printItems(...nums);
+
+const cephalopods = ['dumbo octopus', 'humboldt squid', 'flamboyant cuttlefish'];
+
+const gastropods = ['giant african snail', 'banana slug', 'variable neon slug'];
+
+const cnidaria = ['fire coral', 'moon jelly'];
+
+const mollusca = [...cephalopods, ...gastropods];
+console.log(mollusca);
+
+const inverts = [...cnidaria, ...gastropods, ...cephalopods];
+console.log(inverts);
+
+const cnidariaCopy = [...cnidaria]
+console.log(cnidariaCopy);
+
+
+// spread in object literals
+const feline = {
+    legs: 4,
+    family: 'Felidae'
+};
+
+const canine = {
+    family: 'Canine',
+    furry: true
+};
+
+const dog = {
+    ...canine,
+    isPet: true,
+    adorable: true
+}
+
+console.log(dog['family']);
+
+console.log({...[1,2,3]});
