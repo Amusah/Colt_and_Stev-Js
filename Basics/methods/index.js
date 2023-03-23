4d8fac3824a176ce7404ccf53ff56c697d49b175
Amusah <henryamusah@gmail.com>
2023-03-23 14:54:02 +0000

    the this keyword

--- Basics/methods/index.js
+++ Basics/methods/index.js
@@ -10,3 +10,44 @@ const arith = {
 }
 
 console.log(arith.add(2,4));
+
+
+/************THIS KEYWORD*************/
+let sayHi = () => {
+    console.log('HI');
+    console.log(this); /* the this keyword points to 
+                          the current execution contest
+                          which is the window object which
+                          logs it to the console.
+                     */
+}
+sayHi();
+
+
+/*in a method call, the this
+keyword points to the object 
+that called the method, hence
+person{} object logged to console
+                        */
+const person = {
+    fName: 'Henry',
+    lName: 'Amusah',
+    nickName: '64Bit',
+    fullName(){
+        console.log(this);
+        const {fName, lName, nickName} = this; // destructure
+        return `${fName} ${lName} aka ${nickName}`;
+    },
+    printBio(){
+        console.log(this)
+        let fullName = this.fullName();
+        console.log(`${fullName} is a person!`);
+    }
+}
+person.fullName();
+
+const printBio = person.printBio;
+console.log(printBio());
+
+
+// NOTE: The 'this' key word depends on the context in which it is being invoked
