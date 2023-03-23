6e815b72ba7f13391c6f7c4946d4ed7499d290e6
Amusah <henryamusah@gmail.com>
2023-03-23 10:14:55 +0000

    computed porperties

--- Basics/object shorthand properties/index.js
+++ Basics/object shorthand properties/index.js
@@ -11,3 +11,30 @@ const ratings = [4.5, 5.0, 3.44, 4.0, 3.5, 2.8, 3.5];
 
 const stats = getStats(ratings);
 console.log(stats);
+
+
+/**************COMPUTED PROPERTIES**************/
+const role = 'Lead Engineer';
+const person = 'Henry Amusah';
+const duty = 'Autonomous Flight Software Design';
+
+const team = {
+    [person]: role,
+    duty
+};
+console.log(team);
+
+// adding a new property to obj
+// manual
+const addProp = (obj, k, v) => {
+    const copy = {...obj};
+    copy[k] = v;
+    return copy;
+}
+console.log(addProp(team, 'happy', ':)'));
+
+// computed property
+const addNewProp = (obj, k, v) => {
+    return {...obj, [k]: v}
+}
+console.log(addNewProp(team, 'hardwork', 'pays'));
