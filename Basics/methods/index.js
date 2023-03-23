92f38c2b92776bf35f267af698277d802dc2310d
Amusah <henryamusah@gmail.com>
2023-03-23 10:38:10 +0000

    methods

---
+++ Basics/methods/index.js
@@ -0,0 +1,12 @@
+// methods
+// functions in objects are called methods
+
+const arith = {
+    add: (a,b) => (a + b),
+    sub: (a,b) => (a - b),
+    div: (a,b) => (a / b),
+    mul: (a,b) => (a * b),
+    mod: (a,b) => (a % b)
+}
+
+console.log(arith.add(2,4));
