2bb16c6d0946596048eaff0c343610faa28d3b9f
Amusah <henryamusah@gmail.com>
2023-04-24 17:40:25 +0000

    XHR chaining

--- HTTP Requests/xmlHTTP.js
+++ HTTP Requests/xmlHTTP.js
@@ -1,10 +1,26 @@
 const firstReq = new XMLHttpRequest();
 firstReq.addEventListener('load', function(){
-    console.log('IT WORK');
+    console.log('FIRST REQUEST SENT');
     const data = JSON.parse(this.responseText);
-    for(let planets of data.results){
-        console.log(planets.name);
-    }
+    const filmUrl = data.results[0].films[0];
+    const filmReq = new XMLHttpRequest();
+
+    filmReq.addEventListener('load', function(){
+        console.log('SECOND REQUEST SENT');
+        //console.log(this)
+        const filmData = JSON.parse(this.responseText)
+        console.log(filmData.title);
+    });
+    filmReq.addEventListener('error', () => {
+        console.log('ERROR');
+    });
+    
+    filmReq.open('GET', filmUrl)
+    filmReq.send();
+
+    // for(let planets of data.results){
+    //     console.log(planets.name);
+    // }
 });
 firstReq.addEventListener('error', () => {
     console.log('ERROR');
@@ -12,4 +28,4 @@ firstReq.addEventListener('error', () => {
 firstReq.open('GET', 'https://swapi.dev/api/planets/');
 firstReq.send();
 
-console.log('Request Sent');
+//console.log('Request Sent');
