ede7a681263afd2e056bcccd5db3f75e7c084531
Amusah <henryamusah@gmail.com>
2023-04-27 14:47:14 +0000

    updated fetch.js file with previous unrefactored code

--- HTTP Requests/Fetch/fetch.js
+++ HTTP Requests/Fetch/fetch.js
@@ -1,3 +1,28 @@
+// fetch('https://swapi.dev/api/planets/')
+// .then((response) => {
+//     //console.log(response)
+//     if(!response.ok) throw new Error();
+//         return response.json();
+// }).then((data) => {
+//     console.log('First request fetched all planet and extracted a film url from array of objects')
+//     console.log(data.results[7].films[0]);
+//     const filmUrl = data.results[7].films[0];
+//     return fetch(filmUrl);
+// })
+// .then((response) => {
+//    return !response.ok ? new Error('Request failed') : response.json();
+// })
+// .then((data) => {
+//     console.log('Second request fetched a film data and extracted the film title');
+//     console.log(data.title);
+// })
+// .catch(err => {
+//     console.log('SOMETHING WENT WRONG :(');
+//     console.log(err)
+// });
+
+
+// CODE REFACTURING
 const checkStatusAndParse = (response) => {
     if(!response.ok) throw new Error();
         return response.json();
