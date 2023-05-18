294fe62d9e80e9386f999ccb3290dc83d6ec091c
Amusah <henryamusah@gmail.com>
2023-05-18 18:46:15 +0000

    extracting movie statistics

--- Steve/MovieApp/index.js
+++ Steve/MovieApp/index.js
@@ -28,7 +28,7 @@ autoCompleteWidget({
     root: document.querySelector('#left-autocomplete'),
     onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-movie-details'));
+        onMovieSelect(movie, document.querySelector('#left-movie-details'), 'left');
     },
     
 });
@@ -38,12 +38,14 @@ autoCompleteWidget({
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#right-movie-details'));
+        onMovieSelect(movie, document.querySelector('#right-movie-details'), 'right');
     },
 });
 
 // making a follow up request to get additional information about a specific movie
-const onMovieSelect = async (movie, movieElement) => {
+let leftMovieInfo;
+let rightMovieInfo;
+const onMovieSelect = async (movie, movieElement, side) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '7fb5b472',
@@ -53,10 +55,32 @@ const onMovieSelect = async (movie, movieElement) => {
     // console.log(movie)
     // console.log(response.data);
     movieElement.innerHTML = renderMovie(response.data);
+
+    //side === 'left' ? leftMovieInfo = response.data : rightMovieInfo = response.data;
+    if(side === 'left'){
+        leftMovieInfo = response.data;
+    }else{
+        rightMovieInfo = response.data;
+    }
+
+    if(leftMovieInfo && rightMovieInfo){
+        runComparison();
+    }
+}
+
+const runComparison = () => {
+    console.log('time for comparison');
 }
 
 // injecting movie details into the Dom
 const renderMovie = movieDetails => {
+    // '624589231'
+    const dollars = movieDetails.BoxOffice ? parseInt(movieDetails.BoxOffice.replace(/\$/g, '').replace(/,/g, '')) : '';
+    const metaScore = movieDetails.Metascore ? parseInt(movieDetails.Metascore) : '';
+    const imdbRating = movieDetails.imdbRating ? parseFloat(movieDetails.imdbRating) : '';
+    const imdbVotes = movieDetails.imdbVotes ? parseInt(movieDetails.imdbVotes.replace(/,/g, '')) : '';
+    console.log(dollars, metaScore, imdbRating, imdbVotes)
+
     return `
     <article class="media">
     <figure class="media-left">
