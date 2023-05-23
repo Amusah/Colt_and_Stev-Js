6e8ec8b8bfc859bb199aeef916eb74b6e3521e87
Amusah <henryamusah@gmail.com>
2023-05-23 13:48:02 +0000

    canvas walls and mouse events

--- Steve/mazeGame/index.js
+++ Steve/mazeGame/index.js
@@ -1,7 +1,7 @@
-const { World, Engine, Render, Runner, Bodies } = Matter;
-console.log(Matter)
-console.log(Matter.Engine.create())
-console.log(Matter.Render.create({}))
+const { World, Engine, Render, Runner, Bodies, MouseConstraint, Mouse } = Matter;
+console.log(Matter);
+console.log(Matter.Engine.create());
+console.log(Matter.Render.create({}));
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -17,7 +17,17 @@ console.log(render);
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-const shape = Bodies.rectangle(200, 200, 50, 50, {
-    isStatic: true
-});
-World.add(world, shape);
+World.add(world, MouseConstraint.create(engine, {
+    mouse: Mouse.create(render.canvas)
+}));
+
+// Walls
+const walls = [
+    Bodies.rectangle(400, 0, 800, 40, { isStatic: true }),
+    Bodies.rectangle(400, 600, 800, 40, { isStatic: true }),
+    Bodies.rectangle(0, 300, 40, 600, { isStatic: true}),
+    Bodies.rectangle(800, 300, 40, 600, { isStatic: true })
+];
+World.add(world, walls);
+
+World.add(world, Bodies.rectangle(200, 200, 50, 50));
