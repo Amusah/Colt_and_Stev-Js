f625efffaf1781a07d59194f1d19540cd090f016
Amusah <henryamusah@gmail.com>
2023-05-25 14:55:20 +0000

    drawing the ball

--- Steve/mazeGame/index.js
+++ Steve/mazeGame/index.js
@@ -148,6 +148,7 @@ verticals.forEach((row, rowIndex) => {
     });
 });
 
+// Goal
 const goal = Bodies.rectangle(
     width - unitLength / 2,
     height - unitLength / 2,
@@ -159,3 +160,11 @@ const goal = Bodies.rectangle(
     
 );
 World.add(world, goal);
+
+// Ball
+const ball = Bodies.circle(
+    unitLength / 2,
+    unitLength / 2,
+    unitLength / 4
+);
+World.add(world, ball)
