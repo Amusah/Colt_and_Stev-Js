640eeb12f94af78cf0f38b9c923b4e51c9b92905
Amusah <henryamusah@gmail.com>
2023-05-25 13:59:38 +0000

    Drawing horizontal segments

--- Steve/mazeGame/index.js
+++ Steve/mazeGame/index.js
@@ -6,6 +6,7 @@ console.log(Matter.Render.create({}));
 const cells = 3;
 const width = 600;
 const height = 600;
+const unitLength = width / cells;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -107,11 +108,20 @@ const stepThroughCell = (row, column) => {
 };
 stepThroughCell(startRow, startColumn);
 
-horizontals.forEach(row => {
-    row.forEach(open => {
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
         if(open){
             return;
         }
-        const wall = Bodies.rectangle();
-    })
-})
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength / 2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            10,
+            {
+                isStatic: true
+            }
+        );
+        World.add(world, wall)
+    });
+});
