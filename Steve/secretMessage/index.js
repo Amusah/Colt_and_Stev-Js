03c65aee8ee73117d7a79a66e986141fde160e4c
Amusah <henryamusah@gmail.com>
2023-05-29 12:16:58 +0000

    encoding string

--- Steve/secretMessage/index.js
+++ Steve/secretMessage/index.js
@@ -3,5 +3,8 @@ document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
 
     const input = document.querySelector('input');
-    console.log(input.value)
+    console.log(input.value);
+    const encrypted = btoa(input.value);
+
+    document.querySelector('#link-input').value = encrypted;
 });
