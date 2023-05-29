3b3c47beb9e7734a8c9c4aa89b0053f5ab7638c5
Amusah <henryamusah@gmail.com>
2023-05-29 13:17:02 +0000

    generating message url

--- Steve/secretMessage/index.js
+++ Steve/secretMessage/index.js
@@ -3,8 +3,17 @@ document.querySelector('form').addEventListener('submit', e => {
     e.preventDefault();
 
     const input = document.querySelector('input');
-    console.log(input.value);
-    const encrypted = btoa(input.value);
+    if(input.value){
+        console.log(input.value);
+        const encrypted = btoa(input.value);
 
-    document.querySelector('#link-input').value = encrypted;
+        const msgLink = document.querySelector('#link-input')
+        msgLink.value = `${window.location}#${encrypted}`;
+        msgLink.select();
+    } else{
+        console.log('empty')
+    }
+   
+
+    
 });
