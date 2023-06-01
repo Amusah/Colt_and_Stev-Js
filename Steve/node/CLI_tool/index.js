981546a14ae463a99214febb3fce7c10f0424e59
Amusah <henryamusah@gmail.com>
2023-06-01 15:32:36 +0000

    callback patterns in node

--- Steve/node/CLI_tool/index.js
+++ Steve/node/CLI_tool/index.js
@@ -1,1 +1,16 @@
 const fs = require('fs');
+
+fs.readdir('.', (err, filenames) => {
+    // Either
+    // err === error object
+    // OR
+    // err === null, which means everything was successful
+    
+    if (err) {
+        // Handle error
+        console.log(err);
+    }
+
+
+    console.log(filenames)
+});
