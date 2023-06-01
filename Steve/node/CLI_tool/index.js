ff06602737ea4b29f77b6c5e7338b0123406b4a9
Amusah <henryamusah@gmail.com>
2023-06-01 17:31:56 +0000

    linking a project

--- Steve/node/CLI_tool/index.js
+++ Steve/node/CLI_tool/index.js
@@ -1,3 +1,6 @@
+#!/usr/bin/env node
+// the statement above make our file(index.js) executable 
+
 const fs = require('fs');
 
 fs.readdir(process.cwd(), (err, filenames) => {
@@ -18,7 +21,11 @@ fs.readdir(process.cwd(), (err, filenames) => {
 
 /*
     we noticed that the process object was not required
-    in this file.... const process = require('process')
+    in this file.... const process = requ
+
+/*
+    we noticed that the process object was not required
+    in this file.... const process = require('process')ire('process')
     well... it turns out that process is one of the node's 
     global modules.
 */
