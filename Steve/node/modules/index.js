f37ee7bbf0970a058ffaa56acb446650974db533
Amusah <henryamusah@gmail.com>
2023-06-01 11:41:53 +0000

    explanation

--- Steve/node/modules/index.js
+++ Steve/node/modules/index.js
@@ -7,3 +7,13 @@ console.log(counterObject.getCounter());
 const newCounterObject = require('./script');
 
 console.log(newCounterObject.getCounter());
+
+/*
+    in Node, files get required only once when the script
+    is executed for the first time. node has an inbuilt 
+    cache that store the content of the imported/required file
+    as object with the filename as the key and the content
+    of the files as the value. anytime the require function
+    gets invoked again in the script, the content of the require
+    cache is copied over.
+*/
