0b9b318cb35f8b42a18927e868a37defec7311dd
Amusah <henryamusah@gmail.com>
2023-06-02 15:49:26 +0000

    promise based approach

--- Steve/node/CLI_tool/index.js
+++ Steve/node/CLI_tool/index.js
@@ -20,14 +20,16 @@ fs.readdir(process.cwd(), async (err, filenames) => {
         // Handle error
         console.log(err);
     }
+    const statPromises = filenames.map(filename => {
+        return lstat(filename);
+    });
 
-    for(let filename of filenames){
-        try {
-            const stats = await lstat(filename);
-            console.log(filename, stats.isFile());
-        } catch (err) {
-            console.log(err)
-        }
+    const allStats = await Promise.all(statPromises);
+
+    for(let stats of allStats){
+        const index = allStats.indexOf(stats);
+
+        console.log(filenames[index], stats.isFile());
     }
 });
 
