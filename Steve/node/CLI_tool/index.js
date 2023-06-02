d57758bf3e214a134526b09565ef2209e730de72
Amusah <henryamusah@gmail.com>
2023-06-02 16:33:59 +0000

    specifing files and dirctories in the CLI

--- Steve/node/CLI_tool/index.js
+++ Steve/node/CLI_tool/index.js
@@ -3,6 +3,7 @@
 
 const fs = require('fs');
 const util = require('util');
+//const chalk = require('chalk');
 
 // Method 2
 //const lstat = util.promisify(fs.lstat);
@@ -28,8 +29,12 @@ fs.readdir(process.cwd(), async (err, filenames) => {
 
     for(let stats of allStats){
         const index = allStats.indexOf(stats);
-
-        console.log(filenames[index], stats.isFile());
+        
+        if (stats.isFile()){
+            console.log(`${filenames[index]} is a file`);
+        } else {
+            console.log(`${filenames[index]} is a directory`);
+        }
     }
 });
 
