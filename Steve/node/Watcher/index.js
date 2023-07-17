027172843409ab658e5e9b4d92641f13ba6b5b60
Amusah <henryamusah@gmail.com>
2023-07-17 17:12:16 +0000

    big application issues

--- Steve/node/Watcher/index.js
+++ Steve/node/Watcher/index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
 console.log('i was executed');
+
+/*
+    Big Issues of this project
+    -Need to detect file changes or when a file is updated ...use(chokidar package)
+    
+    -be able to provide some help to other users (user guide) ...use(caporal package)
+    
+    -Need to figure out how to execute some JS code from within ...use(STDlib module 'child_process')
+    a JS program
+*/
