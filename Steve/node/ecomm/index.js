ad5a4463918707c0e69a6102a24e122b40fb4e1e
Amusah <henryamusah@gmail.com>
2023-09-26 17:33:29 +0000

    Displaying a simple HTML

--- Steve/node/ecomm/index.js
+++ Steve/node/ecomm/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
