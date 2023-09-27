e1812cbf77e99d637147928e06430d3b01051c1e
Amusah <henryamusah@gmail.com>
2023-09-27 20:40:43 +0000

    globally applying middleware

--- Steve/node/ecomm/index.js
+++ Steve/node/ecomm/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
     res.send(`
@@ -15,25 +18,25 @@ app.get('/', (req, res) => {
     `);
 });
 
-const bodyParser = (req, res, next) => {
-    if(req.method === 'POST'){
-        req.on('data', data => {
-            const parsed = data.toString('utf8').split('&');
-            console.log(parsed)
-            const formData = {};
-            for(let pair of parsed){
-                const [key, value] = pair.split('=');
-                formData[key] = value;
-            }
-            req.body = formData;
-            next();
-        });
-    } else {
-        next();
-    }
-};
+// const bodyParser = (req, res, next) => {
+//     if(req.method === 'POST'){
+//         req.on('data', data => {
+//             const parsed = data.toString('utf8').split('&');
+//             console.log(parsed)
+//             const formData = {};
+//             for(let pair of parsed){
+//                 const [key, value] = pair.split('=');
+//                 formData[key] = value;
+//             }
+//             req.body = formData;
+//             next();
+//         });
+//     } else {
+//         next();
+//     }
+// };
 
-app.post('/', bodyParser, (req, res) => {
+app.post('/', (req, res) => {
     // get access to email and password
     console.log(req.body);
     res.send('Account created successfully');
