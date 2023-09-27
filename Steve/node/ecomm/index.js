b13b0c93611ebfadae264bab9cae5c369d0df006
Amusah <henryamusah@gmail.com>
2023-09-27 00:06:32 +0000

    Parsing Form Data

--- Steve/node/ecomm/index.js
+++ Steve/node/ecomm/index.js
@@ -15,6 +15,21 @@ app.get('/', (req, res) => {
     `);
 });
 
+app.post('/', (req, res) => {
+    // get access to email and password
+    req.on('data', data => {
+        const parsed = data.toString('utf8').split('&');
+        console.log(parsed)
+        const formData = {};
+        for(let pair of parsed){
+            const [key, value] = pair.split('=');
+            formData[key] = value;
+        }
+        console.log(formData);
+    });
+    res.send('Account created successfully');
+});
+
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
